6084b9395667baf732909b66866dd82cbd9d23fb
Priyavardhan01 <109668918+Priyavardhan01@users.noreply.github.com>
2022-07-28 09:11:58 +0530

    Add files via upload

---
+++ practice project 2.1.java
@@ -0,0 +1,18 @@
+package p1;
+public class a{
+    public void methoddefault() {
+	System.out.println("this is default method");
+}
+    public void   methodpublic() {
+		System.out.println("this is public method");
+	}
+    
+	public  void   methodPrivate() {
+		System.out.println("this is private method");
+		
+	}
+    public  void methodProtected() {
+		System.out.println("this is protected method");
+		
+	}
+}
---
+++ practice project 2.2.java
@@ -0,0 +1,12 @@
+package acces2;
+import p1.*;
+public class accessmodifiers extends a{
+	public static void main(String[] args)  {
+		a obj = new a();	
+		 obj.methoddefault();
+		 obj.methodpublic();
+		 obj.methodProtected();
+		 obj.methodPrivate();
+	}
+
+}
